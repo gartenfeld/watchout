909f3711b4345b1e9d85d21ca06ce054ea3ebcc2
David Rosson <david@rosson.com.au>
2015-07-12 21:00:30 -0700

    using d3 native intervals

--- client/watchout.js
+++ client/watchout.js
@@ -1,101 +1,93 @@
 $(document).ready(function(){
 
   var maxX = 800, maxY = 600;
-  var enemies = [];
-  var x;
-  var y;
-  var r = 20;
-  var newEnemy;
+  var x, y, r = 20;
+  var n = 20;
   var collisionCount = 0;
   var currentScore = 0;
   var highScore = 0;
+  var enemies;
 
-
-  var init = function() {
-    for (var i = 0; i < 25; i++) {
-      newEnemy = new Enemy(0, 0, r);
-      enemies.push(newEnemy);
-    }
-  };
-
-  var drawUser = function () {
+  var initUser = function () {
     var user = d3.select('.arena')
     .append('image')
     .attr('id', 'user')
     .attr('xlink:href', 'img/girl.gif')
-    .attr('width', 70)
-    .attr('height', 70)
-    .attr('x', '390')
-    .attr('y', '280');
+    .style({
+      width: 70,
+      height: 70
+    })
+    .attr('x', maxX/2 - 35)
+    .attr('y', maxY/2 - 35);
   };
+
+  var reX = function() {
+    return Math.random() * (maxX - 2 * r);
+  };
+
+  var reY = function() {
+    return Math.random() * (maxY - 2 * r);
+  };
+
+  var initEnemies = function() {
+    enemies = d3.select('.arena')
+    .selectAll('.enemy')
+    .data(d3.range(n))
+    .enter().append('image')
+    .attr('x', reX)
+    .attr('y', reY)
+    .attr('class', 'enemy')
+    .attr('xlink:href', 'img/shuriken.gif')
+    .attr('width', 2*r)
+    .attr('height', 2*r);
+  };
+
+  var move = function(element) {
+    element
+    .transition()
+    .duration(1500)
+    .attr('x', reX)
+    .attr('y', reY)
+    .each('end', function(){
+      move(d3.select(this));
+    });
+  }; 
 
   var drawBlood = function (x, y) {
     var blood = d3.select('.arena')
     .insert('image', '#user')
     .attr('class', 'blood')
     .attr('xlink:href', 'img/blood.png')
-    .attr('width', 40 + Math.random() * 20)
-    .attr('height', 40 + Math.random() * 20)
-    .attr('x', x + 30 * (Math.random() - 0.5))
-    .attr('y', y + 30 * (Math.random() - 0.5))
+    .attr('width', 35 + Math.random() * 30)
+    .attr('height', 35 + Math.random() * 30)
+    .attr('x', x + 30 * (Math.random() - 0.5) - 30)
+    .attr('y', y + 30 * (Math.random() - 0.5) - 30)
     .attr('opacity', 0)
     .transition()
-    .duration(200)
+    .duration(300)
     .attr('opacity', 1);
-  };
-
-  var relocate = function(array){
-    for (var i = 0; i < array.length; i++) {
-      array[i].x = Math.random() * (maxX - 2 * r) + r;
-      array[i].y = Math.random() * (maxY - 2 * r) + r;
-    }
-  };
-
-  var draw = function(data) {
-    var dots = d3.select('.arena').selectAll('circle').data(data);
-    dots.enter()
-    .append('image')
-    .attr('x', function(d) { return d.x; })
-    .attr('y', function(d) { return d.y; })
-    // .attr('r', function(d) { return d.r; })
-    .attr('class', 'enemy')
-    .attr('xlink:href', 'img/shuriken.gif')
-    .attr('width', 40)
-    .attr('height', 40);
-    // .attr('fill', "url(#image)")
-  }
-
-  var update = function(data) {
-    var dots = d3.select('.arena').selectAll('.enemy').data(data);
-    dots
-    .transition()
-    .duration(1500)
-    .attr('x', function(d) { return d.x; })
-    .attr('y', function(d) { return d.y; });
   };
 
   var previousCollision = false;
 
   var checkCollision = function(){
     var user = d3.select('#user');
-    // here only the first item of the D3 array is the actual array of selected elements
     var enemies = d3.selectAll('.enemy')[0];
     var enemy;
-    var userX = parseFloat(user.attr('x'));
-    var userY = parseFloat(user.attr('y'));
-    var enemyX;
-    var enemyY;
-    var dist;
+    var userX = parseFloat(user.attr('x')) + 35;
+    var userY = parseFloat(user.attr('y')) + 35;
+    var enemyX, enemyY;
+    var distX, distY;
 
     var collision = false;
 
     for (var i = 0; i < enemies.length; i++) {
-      // here we need to wrap it again with D3 methods, such as attr
       enemy = d3.select(enemies[i]);
-      enemyX = parseFloat(enemy.attr('x'));
-      enemyY = parseFloat(enemy.attr('y')); //parseFloat
-      dist = Math.pow((enemyX - userX), 2) + Math.pow((enemyY - userY), 2);
-      if(dist < 35*35) { 
+      enemyX = parseFloat(enemy.attr('x')) + 20;
+      enemyY = parseFloat(enemy.attr('y')) + 20;
+      distX = Math.pow((enemyX - userX), 2);
+      distY = Math.pow((enemyY - userY), 2);
+      if(distX < 35*35 && distY < 40*40) { 
         collision = true;
       }
     }
@@ -114,7 +106,7 @@ $(document).ready(function(){
       highScore = currentScore;
       d3.select('.high').text('High Score: ' + highScore.toString());
     }
-  }
+  };
 
   var flashScreen = function(){
     d3.select('.backdrop')
@@ -134,7 +126,7 @@ $(document).ready(function(){
     checkHighScore(currentScore);
     currentScore = 0;
     flashScreen();
-  }
+  };
 
   var drag = d3.behavior.drag()
     .on('drag', function() {
@@ -142,31 +134,18 @@ $(document).ready(function(){
         .attr('y', d3.event.y < maxY - 35 ? ( d3.event.y > 35 ? d3.event.y - 35 : 0) : maxY - 70); 
     });
 
-  var Enemy = function (x,y,r) {
-    this.x = x;
-    this.y = y;
-    this.r = r;
-  };
+  initUser();
+  initEnemies();
 
-  init();
-  drawUser();
-  relocate(enemies);
-  draw(enemies);
+  move(enemies);
 
   var c = d3.select('#user').call(drag);
-  
-  setInterval(function(){
-    relocate(enemies);
-    update(enemies);
-  }, 1500);
+
+  d3.timer(checkCollision);
 
   setInterval(function(){
     currentScore++;
     d3.select('.current').text('Current Score: ' + currentScore.toString());
-  }, 100);
-
-  d3.timer(checkCollision)
-
-  // .update(enemies);
+  }, 100);  
 
 });
